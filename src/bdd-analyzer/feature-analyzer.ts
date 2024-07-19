29babc9014c644d08420328675dbc222b9db641b
namtvn97 <namtvn97@gmail.com>
2024-07-20 04:24:30 +0700

    Add scenario loader

--- src/bdd-analyzer/feature-analyzer.ts
+++ src/bdd-analyzer/feature-analyzer.ts
@@ -1,26 +1,60 @@
-import { getLinesContainsWord } from "src/utils/text";
+import {
+	divideTextToParagraphsStartWithKeyWords,
+	getLinesStartWithWord,
+} from "src/utils/text";
 export class Feature {
-	private scenarios: Scenario[];
+	private scenarios: Scenario[] = [];
 	private text: string;
+	private name = "";
 	constructor(text: string) {
 		this.text = text;
-		this.scenarios = [];
+		this.loadFeatureName();
+		this.loadScenarios();
 	}
-	getFeatureName(): string {
-		const lines = getLinesContainsWord(this.text, "Feature:");
+	loadFeatureName() {
+		const lines = getLinesStartWithWord(this.text, "Feature:");
 		if (lines.length <= 0) {
 			throw new Error("Feature name not found");
 		}
 		if (lines.length > 1) {
 			throw new Error("Not allow more than one feature name");
 		}
-		return lines[0].replace("Feature:", "").trim();
+		this.text = this.text.replace(lines[0], "");
+		this.name = lines[0].replace("Feature:", "").trim();
+	}
+	getName(): string {
+		return this.name;
+	}
+	getScenariosText() {
+		return divideTextToParagraphsStartWithKeyWords(this.text, [
+			"Scenario:",
+			"Scenario Outline:",
+			"BeforeAll:",
+			"BeforeEach:",
+			"AfterAll:",
+			"AfterEach:",
+		]);
+	}
+	loadScenarios() {
+		const scenariosText = this.getScenariosText();
+		for (const text of scenariosText) {
+			const scenario = new Scenario(text);
+			this.scenarios.push(scenario);
+		}
+	}
+	getScenarios(): Scenario[] {
+		return this.scenarios;
 	}
 }
 class Scenario {
 	private steps: Step[];
+	private text: string;
 	constructor(text: string) {
 		this.steps = [];
+		this.text = text;
+	}
+	getText(): string {
+		return this.text;
 	}
 }
 
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,16 +1,55 @@
 function divideTextIntoLines(text: string): string[] {
 	return text.split("\n");
 }
-function getLinesContainingText(lines: string[], text: string): string[] {
+function getLinesFromArrayContainingText(
+	lines: string[],
+	text: string,
+): string[] {
 	return lines.filter((line) => line.includes(text));
 }
-function getLinesContainingWord(lines: string[], word: string): string[] {
+function getLinesFromArrayContainingWord(
+	lines: string[],
+	word: string,
+): string[] {
 	return lines.filter((line) => line.split(" ").includes(word));
+}
+function getLinesFromArrayStartWithWord(
+	lines: string[],
+	word: string,
+): string[] {
+	return lines.filter((line) => line.trim().startsWith(word));
 }
 export function getLinesContainsWord(
 	originalText: string,
 	word: string,
 ): string[] {
 	const lines = divideTextIntoLines(originalText);
-	return getLinesContainingWord(lines, word);
+	return getLinesFromArrayContainingWord(lines, word);
+}
+export function getLinesStartWithWord(
+	originalText: string,
+	word: string,
+): string[] {
+	const lines = divideTextIntoLines(originalText);
+	return getLinesFromArrayStartWithWord(lines, word);
+}
+export function divideTextToParagraphsStartWithKeyWords(
+	text: string,
+	word: string[],
+): string[] {
+	const lines = divideTextIntoLines(text);
+	const paragraphs: string[] = [];
+	let paragraph = "";
+	for (const line of lines) {
+		const _line = line.trim();
+		if (word.some((w) => _line.startsWith(w))) {
+			if (paragraph !== "") {
+				paragraphs.push(paragraph);
+			}
+			paragraph = line;
+		} else {
+			paragraph += `\n${_line}`;
+		}
+	}
+	return paragraphs;
 }
